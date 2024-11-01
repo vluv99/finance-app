1ed7b7c0e71e3c9d21b12900f368ab13dfef2115
vluv99 <v.luva99@gmail.com>
2024-11-01 22:24:21 +0100

    small fixes

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ import { WelcomeMsg } from "@/components/welcome-msg";
 
 function Header() {
   return (
-    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 ;g:px-14 lg:pb-36">
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 lg:pb-36">
       <div className="max-w-screen-2xl mx-auto">
         <div className="w-full flex items-center justify-between mb-14">
           <div className="flex items-center lg:gap-x-16">
