f8b65f583599dfcdcf15715f2803d0adf2d90d7b
vluv99 <v.luva99@gmail.com>
2024-11-01 11:04:20 +0100

    Hono multi-route & api auth examples

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,52 +1,34 @@
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
+// import { z } from "zod";
+// import { zValidator } from "@hono/zod-validator";
 
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+// import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+
+import authors from "./authors";
+import books from "./books";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app
-  .get("/hello", (c) => {
-    return c.json({
-      message: "Hello Next.js!",
-    });
-  })
-  .get(
-    "/hello/:test",
-    zValidator(
-      "param",
-      z.object({
-        test: z.string(),
-      }),
-    ),
-    (c) => {
-      const { test } = c.req.valid("param");
-      return c.json({
-        message: "Hello World",
-        test: test,
-      });
-    },
-  )
-  .post(
-    "/",
-    zValidator(
-      "json",
-      z.object({
-        name: z.string(),
-        userId: z.string(),
-      }),
-    ),
-    (c) => {
-      const { name, userId } = c.req.valid("json");
-      return c.json({
-        name: name,
-        userId: userId,
-      });
-    },
-  );
+app.route("/authors", authors);
+app.route("/books", books);
+
+// Example of authenticated api request - but the file would be really cluttered if all is collected here
+// app.get("/hello", clerkMiddleware(), (c) => {
+//   const auth = getAuth(c);
+//
+//   if (!auth?.userId) {
+//     return c.json({
+//       message: "Unauthrized",
+//     });
+//   }
+//   return c.json({
+//     message: "Hello Next.js!",
+//     userId: auth.userId,
+//   });
+// });
 
 export const GET = handle(app);
 export const POST = handle(app);
