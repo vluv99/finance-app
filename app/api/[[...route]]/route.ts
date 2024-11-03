508f71f21ccb469e3df88a54d35802d57a1ab9e7
vluv99 <v.luva99@gmail.com>
2024-11-03 14:07:57 +0100

    removed unnecessary HTTPException
    - it was fixed in later hono version

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,19 +2,10 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import accounts from "@/app/api/[[...route]]/accounts";
-import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
-
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse();
-  }
-
-  return c.json({ error: "Internal error" }, 500);
-});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app.route("/accounts", accounts);
