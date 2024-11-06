96e6fec9cb0c8ab13143a72bb8a28ca5f900f19d
vluv99 <v.luva99@gmail.com>
2024-11-06 19:44:17 +0100

    Created & modified DataTable
    - its reusable
    - currently working with mock data
    - added extra functionalities (Pagination, Sorting, Filtering, Row Selection)

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,9 +1,27 @@
 "use client";
 
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
+import { DataTable } from "@/components/data-table";
+import { columns, Payment } from "@/app/(dashboard)/accounts/columns";
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728dsf52f",
+    amount: 56,
+    status: "processing",
+    email: "asd@example.com",
+  },
+];
 
 function AccountsPage() {
   const newAccount = useNewAccount();
@@ -17,6 +35,9 @@ function AccountsPage() {
             Add new
           </Button>
         </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} filterKey="email" />
+        </CardContent>
       </Card>
     </div>
   );
