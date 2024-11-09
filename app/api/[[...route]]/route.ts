4bb6fe71a7ca170484a3693dec17dcd3df7a12b0
vluv99 <v.luva99@gmail.com>
2024-11-09 19:42:48 +0100

    categories schema and api requests added

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,13 +2,16 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import accounts from "@/app/api/[[...route]]/accounts";
+import categories from "@/app/api/[[...route]]/categories";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const routes = app.route("/accounts", accounts);
+const routes = app
+  .route("/accounts", accounts)
+  .route("/categories", categories);
 
 export const GET = handle(app);
 export const POST = handle(app);
