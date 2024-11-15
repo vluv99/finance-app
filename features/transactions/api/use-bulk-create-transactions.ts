ba2e991906287ee6e055356e4330390aaf0ca31e
vluv99 <v.luva99@gmail.com>
2024-11-15 14:58:24 +0100

    Added bulk-create transactions api query & hook

---
+++ features/transactions/api/use-bulk-create-transactions.ts
@@ -0,0 +1,32 @@
+import { InferRequestType, InferResponseType } from "hono";
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.transactions)["bulk-create"]["$post"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.transactions)["bulk-create"]["$post"]
+>["json"];
+
+export const useBulkCreateTransactions = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.transactions["bulk-create"]["$post"]({
+        json,
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Transactions created");
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+      //TODO: also invalidate summaries
+    },
+    onError: () => {
+      toast.error("Failed to create transactions");
+    },
+  });
+};
