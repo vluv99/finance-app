6a92c07e31e0377af7b10a1585e532eaf7788228
vluv99 <v.luva99@gmail.com>
2024-11-17 23:04:49 +0100

    table-head-select component to select column for dataType after import

---
+++ app/(dashboard)/transactions/table-head-select.tsx
@@ -0,0 +1,61 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { cx } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  columnIndex: number;
+  selectedColumns: Record<string, string | null>;
+  onChange: (columnsIndex: number, value: string | null) => void;
+};
+
+const options = ["amount", "date", "payee", "notes"];
+
+export function TableHeadSelect({
+  columnIndex,
+  selectedColumns,
+  onChange,
+}: Props) {
+  const currentSelection = selectedColumns[`column_${columnIndex}`];
+
+  return (
+    <div>
+      <Select
+        value={currentSelection || ""}
+        onValueChange={(value) => onChange(columnIndex, value)}
+      >
+        <SelectTrigger
+          className={cn(
+            "focus:ring-offset-0 focus:ring-transparent outline-none border-none bg-transparent capitalize",
+            currentSelection && "text-blue-500",
+          )}
+        >
+          <SelectValue placeholder="Skip" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value={"skip"}>Skip</SelectItem>
+          {options.map((option, index) => {
+            const disabled =
+              Object.values(selectedColumns).includes(option) &&
+              selectedColumns[`column_${columnIndex}`] !== option;
+            return (
+              <SelectItem
+                value={option}
+                key={index}
+                disabled={disabled}
+                className="capitalized"
+              >
+                {option}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
