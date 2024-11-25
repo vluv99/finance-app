364a28b761347306f73596ff60d4514330cd075f
vluv99 <v.luva99@gmail.com>
2024-11-25 18:42:37 +0100

    small fixes & AreaChart added to dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { DataGrid } from "@/components/data-grid";
+import { DataCharts } from "@/components/data-charts";
 
 function Home() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <DataGrid />
+      <DataCharts />
     </div>
   );
 }
