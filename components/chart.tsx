11a9fa3ce9364c3fe2ecf8cb31a09cc49432926f
vluv99 <v.luva99@gmail.com>
2024-11-26 15:08:45 +0100

    Create lineChart variant for dashboard

--- components/chart.tsx
+++ components/chart.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { FileSearch } from "lucide-react";
 import { AreaVariant } from "@/components/area-variant";
 import { BarVariant } from "@/components/bar-variant";
+import { LineVariant } from "@/components/line-variant";
 
 type Props = {
   data?: {
@@ -28,7 +29,8 @@ export function Chart({ data = [] }: Props) {
           </div>
         ) : (
           // <AreaVariant data={data} />
-          <BarVariant data={data} />
+          // <BarVariant data={data} />
+          <LineVariant data={data} />
         )}
       </CardContent>
     </Card>
