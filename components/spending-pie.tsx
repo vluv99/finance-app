e9fcedd3d7381fb9cbde0727dd9ba20087ef8aa8
vluv99 <v.luva99@gmail.com>
2024-11-26 15:51:00 +0100

    Created pieChart for categories
    + select for different variant
    + custom tooltip for pieChart

---
+++ components/spending-pie.tsx
@@ -0,0 +1,79 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { AreaVariant } from "@/components/area-variant";
+import { BarVariant } from "@/components/bar-variant";
+import { LineVariant } from "@/components/line-variant";
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { FileSearch, PieChart, Radar, Target } from "lucide-react";
+import { PieVariant } from "@/components/pie-variant";
+
+type Props = {
+  data?: {
+    name: string;
+    value: number;
+  }[];
+};
+
+export function SpendingPie({ data = [] }: Props) {
+  const [chartType, setChartType] = useState("pie");
+
+  const onTypeChange = (type: string) => {
+    //TODO: Add paywall
+    setChartType(type);
+  };
+
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
+        <CardTitle className="text-xl line-clamp-1">Categories</CardTitle>
+        <Select defaultValue={chartType} onValueChange={onTypeChange}>
+          <SelectTrigger className="lg:w-auto h-9 rounded-md px-3">
+            <SelectValue placeholder="Chart type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="pie">
+              <div className="flex flex-center">
+                <PieChart className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Pie chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="radar">
+              <div className="flex flex-center">
+                <Radar className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Radar chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="radial">
+              <div className="flex flex-center">
+                <Target className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Radial chart</p>
+              </div>
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent>
+        {data?.length === 0 ? (
+          <div className="flex flex-col gap-y-4 items-center justify-center h-[350px] w-fill">
+            <FileSearch className="size-6 text-muted-foreground" />
+            <p className="text-muted-foreground text-sm">
+              No data for this period
+            </p>
+          </div>
+        ) : (
+          <>
+            {chartType === "pie" && <PieVariant data={data} />}
+            {/*{chartType === "radar" && <BarVariant data={data} />}*/}
+            {/*{chartType === "radial" && <LineVariant data={data} />}*/}
+          </>
+        )}
+      </CardContent>
+    </Card>
+  );
+}
